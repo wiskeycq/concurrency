35372be66f8a9b77a6230e7d01c6f172d94d9bf3
wiskeycq <1213284874@qq.com>
2018-05-03 15:49:58 +0800

    计数例子及lombok的引入

---
+++ src/main/java/com/cq/TestController.java
@@ -0,0 +1,22 @@
+package com.cq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Auther: caoqsq
+ * @Date: 2018/5/3 11:32
+ * @Description:
+ */
+@Controller
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+}
