976b32328b0340e9aad77d3c9fd0a03661554add
wiskeycq <1213284874@qq.com>
2018-05-11 16:32:51 +0800

    并发容器

---
+++ src/main/java/com/cq/example/concurrent/ConcurrentHashMapExample.java
@@ -0,0 +1,60 @@
+package com.cq.example.concurrent;
+
+import com.cq.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @Auther: caoqsq
+ * @Date: 2018/5/11 16:20
+ * @Description: 对应于HashMap，ConcurrentHashMap是并发包下的同步类，面试问的多；
+ * 其中HashTable也是HashMap下同步的类；两个有什么区别呢？
+ * Hashtable和ConcurrentHashMap有什么分别呢？它们都可以用于多线程的环境，但是当Hashtable的大小增加到一定的时候，性能会急剧下降，
+ * 因为迭代时需要被锁定很长的时间。因为ConcurrentHashMap引入了分割(segmentation)，不论它变得多么大，仅仅需要锁定map的某个部分，
+ * 而其它的线程不需要等到迭代完成才能访问map。简而言之，在迭代的过程中，ConcurrentHashMap仅仅锁定map的某个部分，而Hashtable则会锁定整个map。
+ */
+
+@Slf4j
+@ThreadSafe
+public class ConcurrentHashMapExample {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    private static Map<Integer, Integer> map = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size:{}", map.size());
+    }
+
+    private static void update(int i) {
+        map.put(i, i);
+    }
+}
